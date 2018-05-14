3166c3f3a5b683b3091b896abc76668ca6ba5f15
Ian Driscoll <ian.driscoll.o@gmail.com>
2018-05-14 18:13:02 -0400

    Add gh-pages

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -13,7 +13,7 @@ export default class OurWedding extends React.Component {
     return (
       <div className="home wow fadeIn" data-wow-delay="0.2s" data-wow-duration="2s">
         <p className="home__title">Ian & Ali</p>
-        <p className="home__date">09 . 24 . 19</p>
+        <p className="home__date">08 . 24 . 19</p>
         <p className="home__location">Morgan Hill, CA</p>
       </div>
     );
